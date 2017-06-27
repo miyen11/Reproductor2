c621b58bc144ca20e164b3647e39374d3d8e425a
sistema03 <cvamedios@debian>
2017-06-27 16:00:14 -0300

    agregue hola mundo

--- app/src/main/java/com/example/michen/reproductor/MainActivity.java
+++ app/src/main/java/com/example/michen/reproductor/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     ListView lv;
     String[] items;
 
+holamundo
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
